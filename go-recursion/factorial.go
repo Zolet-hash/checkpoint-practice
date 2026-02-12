a87af498016b042cdce674ba3b8aec03e7823b5a
Zolet-hash <emmanuelsulwe@gmail.com>
2026-02-12 20:27:28 +0000

    Update factorial.go

--- go-recursion/factorial.go
+++ go-recursion/factorial.go
@@ -3,9 +3,11 @@ package main
 import ("fmt")
 
 func factorial(n int) int {
+	//base case
 		if n == 0 {
 			return 1
 		}
+	//calling factoral func recurively until it reaches the base case
 		return n * factorial(n-1)
 }
 
